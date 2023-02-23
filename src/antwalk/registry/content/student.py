9032d436be59dd10eaf19a0a45e060cc5618148c
tareq <tareq@zitelab.dk>
2023-02-23 11:22:30 +0100

    added schema

--- src/antwalk/registry/content/student.py
+++ src/antwalk/registry/content/student.py
@@ -8,12 +8,19 @@ from plone.supermodel import model
 
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
 
-# from antwalk.registry import _
+from antwalk.registry import _
 
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+vocab_gender = SimpleVocabulary([
+    SimpleTerm(value='--NOVALUE--', title=_(u'Please Select')),
+    SimpleTerm(value='male', title=_(u'Male')),
+    SimpleTerm(value='female', title=_(u'Female')),
+])
 
 class IStudent(model.Schema):
     """Marker interface and Dexterity Python Schema for Student"""
@@ -22,6 +29,19 @@ class IStudent(model.Schema):
     # and customize it in Python:
 
     # model.load('student.xml')
+
+    studentName = schema.TextLine(
+        title=_(u'Name of student'),
+        required=True
+    )
+
+    age = schema.TextLine(
+        title=_(u'Age of student')
+    )
+    gender = schema.Choice(
+        title=_(u'Gender'),
+        vocabulary=vocab_gender
+    )
 
     # directives.widget(level=RadioFieldWidget)
     # level = schema.Choice(
